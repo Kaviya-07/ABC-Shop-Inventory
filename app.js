818599000a2d671d08c2c84223d8a584d55d4cdd
unknown <kaviya10504@gmail.com>
2025-06-11 18:16:56 +0530

    Updated login API and fixed MongoDB connection error

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ app.set('view engine', 'hbs');
 const itemRoutes = require('./routes/itemRoutes');
 app.use('/', itemRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch((err) => console.log('❌ MongoDB connection error:', err));
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.log("❌ MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
